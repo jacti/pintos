f2fc2f502061bc2be77b2aa4cb9bb85e456986a8
JunPyo0117 <vksehfkdydy@naver.com>
2025-07-14 10:35:48 +0900

    Fix: thread_priority_less 전역 선언 sema_down 수정

--- threads/synch.c
+++ threads/synch.c
@@ -66,7 +66,9 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		// $feat/thread_priority_less
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, thread_priority_less, NULL);
+		// feat/thread_priority_less
 		thread_block ();
 	}
 	sema->value--;
